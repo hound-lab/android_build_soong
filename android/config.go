dd8ebb0d57f6f5861aaacbaeddd28ca38c096664
gutssnv <decryptor69@gmail.com>
2026-06-18 19:25:08 +0000

    Revert "config: move to our keys path"
    This reverts commit 14296426649c233cb8a56179e431a2473b7aad2e.

--- android/config.go
+++ android/config.go
@@ -1440,7 +1440,7 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	if strings.HasPrefix(defaultCert, "vendor/private/") {
+	if strings.HasPrefix(defaultCert, "vendor/lineage-priv/") {
 		return "release-keys"
 	}
 	return "dev-keys"
